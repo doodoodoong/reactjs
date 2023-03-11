57dcab98a22cac6b3c6fbe737fd02bc8f15d7ab2
doodoodoong <doo_doong@hanmail.net>
2023-03-12 00:17:33 +0900

    get movie information

---
+++ nomad-coder/lesson7/movie/src/App.js
@@ -0,0 +1,20 @@
+// 1. home.js에서 초기페이지 만들기
+// 2. movie.js에서 api로 영화정보 불러오는 페이지 만들기
+// 2. 영화 상세정보 detail.js에서 보여주기
+// api `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- nomad-coder/lesson7/movie/src/routes/Home.js
+++ nomad-coder/lesson7/movie/src/routes/Home.js
@@ -3,12 +3,35 @@ import { useState } from "react";
 
 function Home() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setLoading(false);
+    setMovies(json.data.movies);
+  };
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    ).then((res) => console.log(res.data.movies));
+    getMovie();
   }, []);
-  return <div>{loading ? <h1>Loading.....</h1> : null}</div>;
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading.....</h1>
+      ) : (
+        movies.map((movie) => (
+          <div>
+            <img src={movie.medium_cover_image} alt={movie.id} />
+            <p>{movie.title}</p>
+            <p>Summary : {movie.summary}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default Home;
